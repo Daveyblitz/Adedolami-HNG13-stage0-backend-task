814b31cb38159d5ea90a0b7f14aba3f4b461f613
Dolami <dolami.ayinuola@gmail.com>
2025-10-18 16:47:36 +0100

    reset

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.get("/me", async (req, res) => {
-    const PROFILE_EMAIL =  process.env.PROFILE_EMAIL || 'Dolami.ayinuola@gmail.com';
-    const PROFILE_NAME = process.env.PROFILE_NAME || 'Dolami Ayinuola';
-    const PROFILE_STACK = process.env.PROFILE_STACK || 'Node.js/Express';
+    const PROFILE_EMAIL =  process.env.PROFILE_EMAIL || 'youremail@gmail.com';
+    const PROFILE_NAME = process.env.PROFILE_NAME || 'name';
+    const PROFILE_STACK = process.env.PROFILE_STACK || 'tech stack';
 
     const result = {
         status: 'success',
